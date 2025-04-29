73f838b07d003d8101cd2ecb6e1a071b909335a3
ridvanceylan <ridvan.ceylan@msn.com>
2025-04-30 02:37:13 +0300

    cleanup.

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    setupFilesAfterEnv: [
-      '@testing-library/jest-dom', 
-    ],
-    testEnvironment: 'jest-environment-jsdom',
-    setupFiles: ['<rootDir>/jest.setup.js'],
-
-  };
