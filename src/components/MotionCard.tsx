e8a6be4948a61b1392c51bf1aa66df2572449484
Ridvan Ceylan <ridvan.ceylan@msn.com>
2025-04-30 15:22:02 +0300

    animation refactored-1.

--- src/components/MotionCard.tsx
+++ src/components/MotionCard.tsx
@@ -12,15 +12,15 @@ const MotionCard: React.FC<MotionCardProps> = ({ children, className }) => {
       className={className}
       initial={{
         opacity: 0,
-        scale: 0.8,
+        y: 20, 
       }}
       animate={{
         opacity: 1,
-        scale: 1,
+        y: 0,
       }}
       transition={{
-        duration: 0.4,
-        ease: "easeOut",
+        duration: 0.5,
+        ease: "easeInOut",
       }}
     >
       {children}
